f61584093876082d25581655e0d2b0250d801808
Juan Pablo <jpmarquitectura@gmail.com>
2024-08-27 21:45:04 -0300

    commit Initial

---
+++ Clase1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejercicio 1 - Asignacion basica");
+Console.WriteLine("");
+
+Console.WriteLine("A");
+int x = 10;
+int y = 20;
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine("B");
+x = x + 5;
+y = y + 10;
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine("C");
+x = x - 5;
+y = y - 10;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+Console.WriteLine("D");
+x = x * 3;
+y = y * 5;
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine("E");
+x = x / 2;
+y = y / 4;
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine("Presione una tecla para continuar");
+Console.ReadLine();
